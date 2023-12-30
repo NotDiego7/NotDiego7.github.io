b49da91fad51cfcd8cdcf0df564519f09bc5d7a7
NotDiego7 <Lopez.d7@outlook.com>
2023-12-30 12:58:26 -0600

    Progress

--- assets/js/ai.chat.js
+++ assets/js/ai.chat.js
@@ -19,6 +19,9 @@ uploadButton.addEventListener('click', () => {
   input.addEventListener('change', () => {
     fileReader.readAsDataURL(input.files[0]); // Read as data URL for display
 
+    // Disable further file selection
+    input.disabled = true;
+
     // Upload image directly to Dropbox when the send button is clicked
     const sendMessageButton = document.getElementById('send-message-button');
     sendMessageButton.addEventListener('click', async () => {
@@ -26,13 +29,18 @@ uploadButton.addEventListener('click', () => {
       const reader = new FileReader();
       reader.readAsArrayBuffer(imageFile);
 
+
+      let accessToken; // Store the access token globally
+      accessToken = process.env.DBXACCESSTOKEN;
+
       reader.onloadend = async () => {
         try {
+
           // Upload image to Dropbox
           const response = await fetch('https://content.dropboxapi.com/2/files/upload', {
             method: 'POST',
             headers: {
-              'Authorization': 'Bearer sl.BslyUO7x1J4eztf3cvcwB15axP4Daw7684VSEL7cs6Q71Q5q26DrCI_j4DvPeYoiwEE-VuISwxGrCtFF5LDz8a6zalP62X_PgmPddgZ_vI0H5iI5bEbgGTcvYGRef16uJ5UhyD6u8fpuEGaguQI4', 
+              'Authorization': `Bearer ${accessToken}`,
               'Content-Type': 'application/octet-stream',
               'Dropbox-API-Arg': JSON.stringify({
                 path: '/image.jpg',
@@ -42,12 +50,11 @@ uploadButton.addEventListener('click', () => {
             body: reader.result
           });
 
-
           // Get temporary link using /get_temporary_link
           const getTemporaryLinkResponse = await fetch('https://api.dropboxapi.com/2/files/get_temporary_link', {
             method: 'POST',
             headers: {
-              'Authorization': 'Bearer sl.BslyUO7x1J4eztf3cvcwB15axP4Daw7684VSEL7cs6Q71Q5q26DrCI_j4DvPeYoiwEE-VuISwxGrCtFF5LDz8a6zalP62X_PgmPddgZ_vI0H5iI5bEbgGTcvYGRef16uJ5UhyD6u8fpuEGaguQI4',
+              'Authorization': `Bearer ${accessToken}`,
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
@@ -55,15 +62,12 @@ uploadButton.addEventListener('click', () => {
             })
           });
 
-          const dropboxTemporaryLinkResponse = await getTemporaryLinkResponse.json();
-          const temporaryLink = dropboxTemporaryLinkResponse.link;  // Get temporary link
-
-
+          const temporaryLink = await getTemporaryLinkResponse.json().link; // Get temporary link
 
           const text = document.getElementById('message').value;
 
           // Send request to serverSide proxy (Python) | CORS(request) -> Gemini API -> Response
-          const pyProxyResponse = await fetch('https://notdiego7.pythonanywhere.com/', {
+          const pyProxyResponse = await fetch('https://notdiego7.pythonanywhere.com', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
@@ -73,9 +77,7 @@ uploadButton.addEventListener('click', () => {
               temporaryLink: temporaryLink
             })
           });
-          const ProxyResponse = await pyProxyResponse.json();
-          const generatedText = ProxyResponse.text;
-
+          const generatedText = await pyProxyResponse.json().text;
 
           document.querySelector('p#ai-body-text').innerText = generatedText;
         } catch (error) {
@@ -85,3 +87,114 @@ uploadButton.addEventListener('click', () => {
     });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const uploadButton = document.getElementById('upload-image-button');
+
+// uploadButton.addEventListener('click', () => {
+//   const fileReader = new FileReader();
+
+//   // Function to handle the image data for display
+//   fileReader.onload = (event) => {
+//     const imageURL = event.target.result;
+//     const imageElement = document.getElementById('image-preview');
+//     imageElement.src = imageURL;
+//   };
+
+//   // Trigger the file selection dialog
+//   const input = document.createElement('input');
+//   input.type = 'file';
+//   input.accept = 'image/*';
+//   input.click();
+
+//   input.addEventListener('change', () => {
+//     fileReader.readAsDataURL(input.files[0]); // Read as data URL for display
+
+//     // Send image bytes as a Blob when the send button is clicked
+//     const sendMessageButton = document.getElementById('send-message-button');
+//     sendMessageButton.addEventListener('click', () => {
+//       const formData = new FormData();
+//       formData.append('text', document.getElementById('message').value);
+
+//       // Read image bytes directly
+//       const imageFile = input.files[0];
+//       const reader = new FileReader();
+//       reader.readAsArrayBuffer(imageFile);
+
+//       reader.onloadend = () => {
+//         formData.append('imageBytes', new Blob([reader.result])); // Append image bytes as a Blob
+
+//         fetch('https://notdiego7.pythonanywhere.com/', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'multipart/form-data'
+//           },
+//           body: formData
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//           document.querySelector('p#ai-body-text').innerText = data.text;
+//         })
+//         .catch(error => {
+//           console.error('Error sending image data:', error);
+//         });
+//       };
+//     });
+//   });
+// });
